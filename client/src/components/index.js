d2bb42ac39efd07f83b7d3f288e1eb9342216a28
Daren <darenleongzy@gmail.com>
2020-06-13 16:19:36 +0800

    Added blog home page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,5 @@
 export { default as App } from './App';
+export { default as Home } from './Home';
+export { default as Submit } from './Submit';
+export { default as Article } from './Article';
+export { default as AppBar } from './AppBar';
