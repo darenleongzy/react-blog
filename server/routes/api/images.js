916cb4ea182b4e484ad9f608b5b23e767081b373
Daren <darenleongzy@gmail.com>
2021-03-24 22:29:21 +0800

    Added google cloud store module and usage

--- server/routes/api/images.js
+++ server/routes/api/images.js
@@ -4,28 +4,54 @@ const Images = mongoose.model('Images');
 var fs = require('fs');
 var path = require('path');
 require('dotenv/config');
+const {Storage} = require('@google-cloud/storage');
 
-var multer = require('multer');
+const storage = new Storage();
+var Multer = require('multer');
  
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.resolve("./images/"))
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+      fileSize: 16 * 1024 * 1024, // no larger than 16mb, you can change as needed.
     },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + '.jpg')
-    }
 });
  
-var upload = multer({ storage: storage });
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
-router.post('/', upload.single('image'), (req, res, next) => {
+
+router.post('/', multer.single('image'), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  }
   console.log("image req:", req);
   var imagePath = req.file.filename;
-  const newImage = new Images({
-    imagePath: imagePath,
+
+    // Create a new blob in the bucket and upload the file data.
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream({
+    resumable: false,
   });
-  return newImage.save()
-    .then(() => res.json({ image: newImage.toJSON() }))
+
+  blobStream.on('error', err => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    );
+    res.status(200).send(publicUrl);
+  });
+
+  blobStream.end(req.file.buffer);
+
+  // const newImage = new Images({
+  //   imagePath: imagePath,
+  // });
+  // return newImage.save()
+  //   .then(() => res.json({ image: newImage.toJSON() }))
 });
 
 router.get('/', (req, res, next) => {
