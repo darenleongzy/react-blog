8a6f3bbb54ec0964e436a2c19e337a43b7b260be
Daren <darenleongzy@gmail.com>
2021-04-21 14:50:57 +0800

    Added dynamic url for react router sitemap

--- client/sitemap-builder.js
+++ client/sitemap-builder.js
@@ -4,5 +4,36 @@ require("@babel/register")({
 
 const router = require("./router").default;
 const Sitemap = require("react-router-sitemap").default;
+const axios = require("axios");
+async function generateSitemap() {
+  try {
+    let urlMap = [];
+    axios
+      .get("https://api-dot-darenleong-webapp.et.r.appspot.com:/api/articles")
+      .then((response) => {
+        response.data.articles.map((article) => {
+          console.log(article.urlTitle);
+          urlMap.push({
+            urlTitle: article.urlTitle,
+            articleId: article._id,
+          });
+        });
+        console.log(urlMap);
+        const paramsConfig = {
+          "/article/:urlTitle/:articleId": urlMap,
+        };
+        return new Sitemap(router)
+          .applyParams(paramsConfig)
+          .build("https://dalezy.com")
+          .save("./dist/sitemap.xml");
+      })
+      .catch((error) => console.log(error.response));
+    // pathname: "/article/" + urlTitle + "/" + data.articles[index]._id,
 
-new Sitemap(router).build("https://dalezy.com").save("./dist/sitemap.xml");
+    //    pathname: "/article/" + urlTitle + "/" + data.articles[index]._id,
+    //    state: { article: data.articles[index] },
+  } catch (e) {
+    console.log(e);
+  }
+}
+generateSitemap();
